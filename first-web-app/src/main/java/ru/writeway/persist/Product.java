0a084d522fcfb311866a7b53e9f25d610386044a
eska2000 <eska2000@gmail.com>
2021-02-11 17:05:07 +0300

    lesson 3

---
+++ first-web-app/src/main/java/ru/writeway/persist/Product.java
@@ -0,0 +1,50 @@
+package ru.writeway.persist;
+
+import java.math.BigDecimal;
+
+public class Product {
+    protected Long id;
+    protected BigDecimal price;
+    protected String description;
+    protected String name;
+
+    public Long getId() {
+        return id;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Product(Long id, String name, String description, BigDecimal price) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.price = price;
+    }
+
+}
